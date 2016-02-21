b14fa6d132fd7832efe11f6a0a39134b198e90cf
hugowangguowei <wang876856259@126.com>
2016-02-21 20:44:54 +0800

    修改baGlobal

--- baBasicLib/gameState.js
+++ baBasicLib/gameState.js
@@ -2,146 +2,149 @@
  * Created by wangguowei on 2001/1/1.
  */
 
-function gameStateMachine(){
-    this.obj = "";
-    this.gameStateArray = [];
-    this.currentState = null;
-    this.chosenObj = null;
-    this.userID = "hugo";
-    this.currentObj = null;
-    this.privateState = {};
-    this._startGameMsg = null;
-}
-gameStateMachine.prototype = {
-    addListener:function(){
-        //!!
-        this.obj.baseDiv.addEventListener("mousedown",gameStateMachine.prototype.doMouseDown,false);
-        this.obj.baseDiv.addEventListener("mousemove",gameStateMachine.prototype.doMouseMove,false);
-        this.obj.baseDiv.addEventListener("mouseup",gameStateMachine.prototype.doMouseUp,false);
-        this.obj.baseDiv.onmousewheel = this.doMouseWheel;
-        this.obj.baseDiv.oncontextmenu = function(event){
-            var event = event||window.event;
-            //!!
-            this.baGlobal.GSM.mouseEventHandle("rightMouseDown",event);
-            return false;
-        }
-        document.onkeydown = this.onKeyDown;
-        document.onkeyup = this.onKeyUp;
-    },
-    onKeyDown: function (e) {
-        var keyCode = e.which;
-        var realKey = String.fromCharCode(keyCode);
-        global.GSM.keyEvent({type:'keyDown',detail:realKey});
-    },
-    onKeyUp:function(e){
-        var keyCode = e.which;
-        var realKey = String.fromCharCode(keyCode);
-        global.GSM.keyEvent({type:'keyUp',detail:realKey});
-    },
-    keyEvent:function(e){
+define(function(){
+    function gameStateMachine(){
+        this.obj = "";
+        this.gameStateArray = [];
+        this.currentState = null;
+        this.chosenObj = null;
+        this.userID = "hugo";
+        this.currentObj = null;
+        this.privateState = {};
+        this._startGameMsg = null;
+    }
+    gameStateMachine.prototype = {
+        addListener:function(){
+            this.obj.baseDiv.addEventListener("mousedown",gameStateMachine.prototype.doMouseDown,false);
+            this.obj.baseDiv.addEventListener("mousemove",gameStateMachine.prototype.doMouseMove,false);
+            this.obj.baseDiv.addEventListener("mouseup",gameStateMachine.prototype.doMouseUp,false);
+            this.obj.baseDiv.onmousewheel = this.doMouseWheel;
+            this.obj.baseDiv.oncontextmenu = function(event){
+                var event = event||window.event;
+                //!!
+                this.baGlobal.GSM.mouseEventHandle("rightMouseDown",event);
+                return false;
+            }
+            document.onkeydown = this.onKeyDown;
+            document.onkeyup = this.onKeyUp;
+        },
+        onKeyDown: function (e) {
+            var keyCode = e.which;
+            var realKey = String.fromCharCode(keyCode);
+            global.GSM.keyEvent({type:'keyDown',detail:realKey});
+        },
+        onKeyUp:function(e){
+            var keyCode = e.which;
+            var realKey = String.fromCharCode(keyCode);
+            global.GSM.keyEvent({type:'keyUp',detail:realKey});
+        },
+        keyEvent:function(e){
 
-    },
-    doMouseDown:function(e){
-        global.GSM.mouseEventHandle("mousedown",event);
-    },
-    doMouseMove:function(e){
-        global.GSM.mouseEventHandle("mousemove",event);
-    },
-    doMouseUp:function(e){
-        global.GSM.mouseEventHandle("mouseup",event);
-    },
-    doMouseWheel:function(e){
-        var delta_y = event.deltaY;
-    },
-    getPointOnDiv:function(x,y){
-        var obj = this.obj.baseDiv;
-        var bBox = obj.getBoundingClientRect();
-        return {
-            x:x-bBox.left*(obj.width/bBox.width),
-            y:y - bBox.top*(obj.height/bBox.height)
-        };
-    },
-    getMouseDownObj:function(loc){
-        var reflectArray = [];
-        for(var i in this.obj.layerArray){
-            if(this.obj.layerArray[i].state == "show"){
-                for(var m in this.obj.layerArray[i].childList){
-                    var sp = this.obj.layerArray[i].childList[m];
-                    if(sp.reflectable&&sp.reflectBack(loc)){
-                        reflectArray.push(sp);
+        },
+        doMouseDown:function(e){
+            global.GSM.mouseEventHandle("mousedown",event);
+        },
+        doMouseMove:function(e){
+            global.GSM.mouseEventHandle("mousemove",event);
+        },
+        doMouseUp:function(e){
+            global.GSM.mouseEventHandle("mouseup",event);
+        },
+        doMouseWheel:function(e){
+            var delta_y = event.deltaY;
+        },
+        getPointOnDiv:function(x,y){
+            var obj = this.obj.baseDiv;
+            var bBox = obj.getBoundingClientRect();
+            return {
+                x:x-bBox.left*(obj.width/bBox.width),
+                y:y - bBox.top*(obj.height/bBox.height)
+            };
+        },
+        getMouseDownObj:function(loc){
+            var reflectArray = [];
+            for(var i in this.obj.layerArray){
+                if(this.obj.layerArray[i].state == "show"){
+                    for(var m in this.obj.layerArray[i].childList){
+                        var sp = this.obj.layerArray[i].childList[m];
+                        if(sp.reflectable&&sp.reflectBack(loc)){
+                            reflectArray.push(sp);
+                        }
                     }
                 }
             }
-        }
-        var finalSprite = reflectArray[0];
-        for(var i = 0;i<reflectArray.length;i++){
-            if(reflectArray[i].parent.zindex > finalSprite.parent.zindex){
-                finalSprite = reflectArray[i];
+            var finalSprite = reflectArray[0];
+            for(var i = 0;i<reflectArray.length;i++){
+                if(reflectArray[i].parent.zindex > finalSprite.parent.zindex){
+                    finalSprite = reflectArray[i];
+                }
             }
-        }
-        return finalSprite;
-    },
-    mouseEventHandle:function(eventType,event){
-        var self = this;
-        var loc = this.getPointOnDiv(event.pageX,event.pageY);
-        _mouseEvent(eventType,loc);
+            return finalSprite;
+        },
+        mouseEventHandle:function(eventType,event){
+            var self = this;
+            var loc = this.getPointOnDiv(event.pageX,event.pageY);
+            _mouseEvent(eventType,loc);
 
-        function _mouseEvent(eventType,loc){
-            switch (eventType){
-                case "mousedown":
-                    _mousedown(loc);
-                    break;
-                case "mousemove":
-                    _mousemove(loc);
-                    break;
-                case "mouseup":
-                    _mouseup(loc);
-                    break;
+            function _mouseEvent(eventType,loc){
+                switch (eventType){
+                    case "mousedown":
+                        _mousedown(loc);
+                        break;
+                    case "mousemove":
+                        _mousemove(loc);
+                        break;
+                    case "mouseup":
+                        _mouseup(loc);
+                        break;
+                }
             }
-        }
-        function _mousedown(loc){
-            var obj = self.getMouseDownObj(loc);
-            if(obj){
-                console.log(obj.id);
-                obj.inputEvent("mousedown");
-                self.chosenObj = obj;
+            function _mousedown(loc){
+                var obj = self.getMouseDownObj(loc);
+                if(obj){
+                    console.log(obj.id);
+                    obj.inputEvent("mousedown");
+                    self.chosenObj = obj;
+                }
+                else{
+                    console.log("no obj chosen");
+                }
+            }
+            function _mousemove(loc){
+
+            }
+            function _mouseup(loc){
+                var obj = self.chosenObj;
+                if(obj){
+                    obj.inputEvent("mouseup");
+                    this.chosenObj = 0;
+                }
+            }
+        },
+
+        initRoutine:function(){
+            this.addListener();
+            this.currentState = this.gameStateArray[0];
+            this.currentState.stateFunction(this);
+        },
+        switchToNext:function(stateName){
+            var stateChanged = false;
+            for(var i = 0;i<this.gameStateArray.length;i++){
+                if(this.gameStateArray[i].stateName == stateName){
+                    this.currentState = this.gameStateArray[i];
+                    stateChanged = true;
+                }
+            }
+
+            if(!stateChanged){
+                throw new Error("gsm set wrong");
             }
             else{
-                console.log("no obj chosen");
+                this.currentState.stateFunction(this);
+                console.log("LISTEN: " + this.currentState.stateName);
             }
-        }
-        function _mousemove(loc){
+        },
+    }
+    return
+})
 
-        }
-        function _mouseup(loc){
-            var obj = self.chosenObj;
-            if(obj){
-                obj.inputEvent("mouseup");
-                this.chosenObj = 0;
-            }
-        }
-    },
-
-    initRoutine:function(){
-        this.addListener();
-        this.currentState = this.gameStateArray[0];
-        this.currentState.stateFunction(this);
-    },
-    switchToNext:function(stateName){
-        var stateChanged = false;
-        for(var i = 0;i<this.gameStateArray.length;i++){
-            if(this.gameStateArray[i].stateName == stateName){
-                this.currentState = this.gameStateArray[i];
-                stateChanged = true;
-            }
-        }
-
-        if(!stateChanged){
-            throw new Error("gsm set wrong");
-        }
-        else{
-            this.currentState.stateFunction(this);
-            console.log("LISTEN: " + this.currentState.stateName);
-        }
-    },
-}
--- gameStateMachine/GSM_mainShowBasicStruct.js
+++ gameStateMachine/GSM_mainShowBasicStruct.js
@@ -1,56 +1,59 @@
 /**
  * Created by wangguowei on 2001/1/11.
  */
+define(function(){
+    function GSM_mainShowBasicStruct(_this){
+        _this.privateState.mainSHowBBSOn = true;
+        var mainShowLayer = global.getLayer("mainShowLayer");
+        var singleProBtn = mainShowLayer.getChildById("singleProBtn");
+        mainShowLayer.removeChild(singleProBtn);
 
-function GSM_mainShowBasicStruct(_this){
-    _this.privateState.mainSHowBBSOn = true;
-    var mainShowLayer = global.getLayer("mainShowLayer");
-    var singleProBtn = mainShowLayer.getChildById("singleProBtn");
-    mainShowLayer.removeChild(singleProBtn);
+        var os_1 = new oS_roomList("outerS",300,50,600,350,false);
+        os_1.addToLayer(mainShowLayer);
 
-    var os_1 = new oS_roomList("outerS",300,50,600,350,false);
-    os_1.addToLayer(mainShowLayer);
+        var btn1 = new baButton("btn_mS_buildRoom");
+        var btn1_loc = {
+            x:os_1.x,
+            y:os_1.y + os_1.height,
+            width:os_1.width/4,
+            height:50
+        }
+        btn1.setLoc(btn1_loc);
+        btn1.upStateInfo.text = "buildRoom";
+        btn1.addToLayer(mainShowLayer);
+        os_1.addNode(btn1);
 
-    var btn1 = new baButton("btn_mS_buildRoom");
-    var btn1_loc = {
-        x:os_1.x,
-        y:os_1.y + os_1.height,
-        width:os_1.width/4,
-        height:50
-    }
-    btn1.setLoc(btn1_loc);
-    btn1.upStateInfo.text = "buildRoom";
-    btn1.addToLayer(mainShowLayer);
-    os_1.addNode(btn1);
+        var btn2 = new baButton("btn_mS_pickRoom");
+        var btn2_loc = {
+            x:os_1.x + os_1.width/2 - os_1.width/8,
+            y:os_1.y + os_1.height,
+            width:os_1.width/4,
+            height:50
+        }
+        btn2.setLoc(btn2_loc);
+        btn2.upStateInfo.text = "pickRoom";
+        btn2.addToLayer(mainShowLayer);
+        os_1.addNode(btn2);
+        btn2.bindedEvent = BTN_E_getIntoARoom;
 
-    var btn2 = new baButton("btn_mS_pickRoom");
-    var btn2_loc = {
-        x:os_1.x + os_1.width/2 - os_1.width/8,
-        y:os_1.y + os_1.height,
-        width:os_1.width/4,
-        height:50
-    }
-    btn2.setLoc(btn2_loc);
-    btn2.upStateInfo.text = "pickRoom";
-    btn2.addToLayer(mainShowLayer);
-    os_1.addNode(btn2);
-    btn2.bindedEvent = BTN_E_getIntoARoom;
+        var btn3 = new baButton("btn_mS_randomRoom");
+        var btn3_loc = {
+            x:os_1.x + os_1.width - os_1.width/4,
+            y:os_1.y + os_1.height,
+            width:os_1.width/4,
+            height:50
+        }
+        btn3.setLoc(btn3_loc);
+        btn3.upStateInfo.text = "buildRoom";
+        btn3.addToLayer(mainShowLayer);
+        os_1.addNode(btn3);
 
-    var btn3 = new baButton("btn_mS_randomRoom");
-    var btn3_loc = {
-        x:os_1.x + os_1.width - os_1.width/4,
-        y:os_1.y + os_1.height,
-        width:os_1.width/4,
-        height:50
-    }
-    btn3.setLoc(btn3_loc);
-    btn3.upStateInfo.text = "buildRoom";
-    btn3.addToLayer(mainShowLayer);
-    os_1.addNode(btn3);
+        _this.doMouseWheel = function (event) {
+            var delta_y = event.deltaY;
+            var os_1 = global.getSpriteById("outerS");
+            os_1.onMouseWheel(delta_y);
+        }
+    };
+    return GSM_mainShowBasicStruct;
+})
 
-    _this.doMouseWheel = function (event) {
-        var delta_y = event.deltaY;
-        var os_1 = global.getSpriteById("outerS");
-        os_1.onMouseWheel(delta_y);
-    }
-}
