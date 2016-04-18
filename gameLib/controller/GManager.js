6c45a76afe659b7163760c7c4067e79f93ce58f9
hugowangguowei <wang876856259@126.com>
2016-04-18 22:13:21 +0800

    开始制作地图生成器

---
+++ gameLib/controller/GManager.js
@@ -0,0 +1,35 @@
+/**
+ * Created by wgw on 2016/4/18.
+ */
+define(function(require){
+    function GManager(initInfo){
+        this.geoInfo = {
+            hasMap:false,
+            width:0,
+            height:0,
+            backgroundPic:null,
+            mapArray:[]
+        }
+        this.spriteList = [];
+        this.timer = {
+            timerTask:null,
+            frameSpeed:40
+        }
+        this.initialize(initInfo);
+    }
+
+    GManager.prototype = {
+        initialize:function(initInfo){
+            initGeo();
+            function initGeo(){
+
+            };
+            this.timer.timerTask = setInterval(function(){
+                for(var i = 0;i<this.spriteList.length;i++){
+                    var sprite_i = this.spriteList[i];
+                    sprite_i.action();
+                }
+            },this.timer.frameSpeed);
+        }
+    }
+});
---
+++ gameLib/model/sprite/Sprite.js
@@ -0,0 +1,20 @@
+/**
+ * Created by wgw on 2016/4/18.
+ */
+define(function(require){
+   function Sprite(id){
+       this.id = id;
+       this.geoInfo = {
+           hasGeo:false,
+           GeoLoc:{x:null,y:null}
+       }
+   }
+    Sprite.prototype = {
+        initialize:function(){
+
+        },
+        action:function(){
+
+        }
+    }
+});
